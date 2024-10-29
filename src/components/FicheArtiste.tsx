8a7e8387a09dbd2881d71fa08eea3f8616c8992e
kossi973 <150068989+kossi973@users.noreply.github.com>
2024-10-29 17:38:20 +0100

    Amélioration affichage fiche artiste

--- src/components/FicheArtiste.tsx
+++ src/components/FicheArtiste.tsx
@@ -29,27 +29,26 @@ const FicheArtiste: React.FC<FicheProps> = ({ artiste , eventsArtiste } ) =>  {
       <div className='text-right'>
           <button onClick={OuvrirFiche} className='text-cyan-200 underline font-bold active:text-amber-500 pb-1 pr-1'>Voir +</button>
       </div>
-      <dialog className="overflow-visible bg-hero2 rounded-md text-yellow-100 absolute mt-36 w-full md:w-1/3 shadow-lg shadow-orange-300" ref={dialogRef}>
+      <dialog className="overflow-visible bg-hero2 text-yellow-100 rounded-md absolute mt-36 w-full md:w-1/3 shadow-lg shadow-orange-300" ref={dialogRef}>
 
-        <div className='bg-blue-800/90' >
+        <div className='bg-blue-800/90 rounded-md' >
             <button className='text-md mt-4 ml-4 border-2 rounded-full px-2 active:text-amber-500' onClick={FermerFiche}>X</button>
-            <h1 className='grid justify-center text-3xl mb-8 border-b border-b-yellow-600 pb-4 font-bold'>La story de l'artiste</h1>
-            <div className='text-xl bg-blue-800 mx-10 shadow-lg shadow-cyan-300'>   
+            <h1 className='grid justify-center text-3xl mb-8 border-b pb-4 font-bold'>La story de l'artiste</h1>
+            <div className='text-xl bg-blue-800 rounded-md mx-10 shadow-lg shadow-cyan-300'>   
                 <img src={url_du_visuel} alt={nom_de_lartiste} className='rounded-t w-full mb-4'/>
                 <p className='font-bold pl-2 mb-4'>{nom_de_lartiste}</p>
                 <p className='pl-6 mb-4'>{style_de_lartiste}</p> 
                 <p className='pl-6 mb-5'>{description_de_lartiste}</p>
                 <p className='pl-2 font-bold mb-1'>Programmation</p>
-                <hr className="mb-2"/>
                 <ul className="italic">
                   {eventsArtiste && eventsArtiste.length > 0 ? (
                     eventsArtiste.map((event, index) => (                      
                         <li className="mb-2" key={index}>
+                            <hr className="mt-2 mb-3"/>
                             <div className="ml-2" >
                                 <p className='pl-4'>Jour {event.acf.jour_event} - {event.acf.horaire_event.slice(0,5)}</p>
                                 <p className='pl-4 text-lg'>» {event.acf.event_festival} sur la scène {event.acf.scene_festival}</p>
                             </div>
-                            <hr className="mt-2 mb-4"/>
                         </li>
                       ))) : (
                       <p>No events available</p>
