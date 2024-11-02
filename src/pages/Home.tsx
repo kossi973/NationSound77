8aecc6673485d753390407d2f86c4c2436df8421
kossi973 <150068989+kossi973@users.noreply.github.com>
2024-11-02 22:46:31 +0100

    Update nommage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -68,7 +68,7 @@ function Home() {
                   <Link to={"/Programmation"}>PROGRAMMATION</Link>
                 </button>
                 <button className='h-14 w-full mb-3 md:w-1/2 px-8 mx-auto text-xl font-bold border rounded-lg bg-blue-800/80 shadow-lg shadow-orange-300 hover:ring hover:ring-violet-300 active:shadow-xl active:bg-amber-500'>
-                  <Link to={"/Billeterie"}>BILLETERIE</Link>
+                  <Link to={"/Billeterie"}>BILLETTERIE</Link>
                 </button>
                 <button className='h-14 w-full mb-3 md:w-1/2 px-8 mx-auto text-xl font-bold border rounded-lg bg-blue-800/80 shadow-lg shadow-orange-300 hover:ring hover:ring-violet-300 active:shadow-xl active:bg-amber-500'>
                   <Link to={"/NationMap"}>CARTE INTERACTIVE</Link>
